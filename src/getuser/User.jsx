f72e118056144a19bf615dcadb2a24af34a41fc5
anithaponnu <6006ece12@gmail.com>
2025-10-19 01:13:21 +0000

    user

---
+++ src/getuser/User.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className= "UserTable">
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">S.No.</th>
+          </tr>
+        </thead>
+      </table>
+     
+    </div>
+  )
+}
+
+export default User
